a9cc7f4bdf558e93f2c678b39d138bed4cbf3d3b
Gerard C. Saperas <gcsaperas@gmail.com>
2020-08-18 09:07:08 +0200

    added handlebars

--- src/app.js
+++ src/app.js
@@ -3,9 +3,32 @@ const express = require('express');
 
 const app = express();
 
-// Use the static path of our public folder for the app
 const publicDirectoryPath = path.join(__dirname, '../public');
+
+// Handlebars (hbs)
+app.set('view engine', 'hbs');
+
+// Serve files from public
 app.use(express.static(publicDirectoryPath));
+
+app.get('/', (req, res) => {
+	res.render('index', {
+		title: 'Weather App',
+		name: 'Gerard'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about', {
+		title: 'About'
+	});
+});
+
+app.get('/help', (req, res) => {
+	res.render('help', {
+		title: 'Help'
+	});
+});
 
 // app.com
 // app.com/help
